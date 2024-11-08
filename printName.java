137b62cbd6dba3e349a21fa479fef08fd75158f5
francisr-8 <respondejohnfrancis0@gmail.com>
2024-11-07 23:52:21 -0800

    Add files via upload

---
+++ printName.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class printName
+{
+	public static void main(String[]args) {
+	for (int i = 1; i <= 5000; ++i) {
+		System.out.println("John Francis F. Responde");
+		}
+	}
+}
